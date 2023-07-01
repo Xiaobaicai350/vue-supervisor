eb0ce18d83ffe472f2a357862f4c12d481a3f39c
胥天昊 <1499487526@qq.com>
2023-07-01 14:45:18 +0800

    初始化走马灯

--- src/views/Estimated/index.vue
+++ src/views/Estimated/index.vue
@@ -1,9 +1,43 @@
-<template>
-  <div>我是Estimated</div>
-</template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      imgwrap: [
+        { url: require("@/assets/imgs/wuran1.png") },
+        { url: require("@/assets/imgs/wuran2.png") },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<template>
+  <div>
+    <h1>可以根据下方的图片进行预估AQI等级</h1>
+    <div class="block">
+      <el-carousel trigger="click" height="600px">
+        <el-carousel-item v-for="item in imgwrap" :key="item.url">
+          <img :src="item.url" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>

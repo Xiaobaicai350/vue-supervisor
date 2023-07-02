8cd86f737e39f9dd40acabe1e57a2f108aeac15d
胥天昊 <1499487526@qq.com>
2023-07-02 20:32:13 +0800

    列出自己提交的异常信息

--- src/apis/supervisor.js
+++ src/apis/supervisor.js
@@ -13,3 +13,12 @@ export const insertSuperviseInfo = ({ address, vAQILevel, description }) => {
     },
   });
 };
+
+// 添加举报信息
+//这里的id是写死的，之后再修改
+export const ListExInformation = () => {
+  return request({
+    url: "/supervisor?id=1",
+    method: "GET",
+  });
+};
--- src/views/ListExInformation/index.vue
+++ src/views/ListExInformation/index.vue
@@ -1,12 +1,19 @@
+<!-- 这个页面主要用来列出已提交的异常信息 -->
 <template>
   <el-table :data="tableData" border stripe style="width: 100%">
-    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-    <el-table-column prop="address" label="地址"> </el-table-column>
+    <el-table-column prop="id" label="id"> </el-table-column>
+    <el-table-column prop="address" label="address"> </el-table-column>
+    <el-table-column prop="description" label="description"> </el-table-column>
+    <el-table-column prop="status" label="status"> </el-table-column>
+    <el-table-column prop="supervisorId" label="supervisorId">
+    </el-table-column>
+    <el-table-column prop="updateTime" label="updateTime"> </el-table-column>
+    <el-table-column prop="vAQILevel" label="vAQILevel"> </el-table-column>
   </el-table>
 </template>
 
 <script>
+import { ListExInformation } from "@/apis/supervisor.js";
 export default {
   data() {
     return {
@@ -33,6 +40,14 @@ export default {
         },
       ],
     };
+  },
+  async mounted() {
+    const data = await ListExInformation();
+    console.log(data);
+    for (let i = 0; i < data.data.length; i++) {
+      data.data[i].updateTime = data.data[i].updateTime.substring(0, 19);
+    }
+    this.tableData = data.data;
   },
 };
 </script>

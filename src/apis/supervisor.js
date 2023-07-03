70c82198aa78b30504e1f7693320195120799f2b
胥天昊 <1499487526@qq.com>
2023-07-03 21:05:34 +0800

    m

--- src/apis/supervisor.js
+++ src/apis/supervisor.js
@@ -1,36 +1,6 @@
 // 封装举报人相关接口
 import request from "@/utils/http";
 
-// 添加举报信息
-//这里的id是写死的，之后再修改
-
-export const insertSuperviseInfo = ({
-  supervisorId,
-  address,
-  vAQILevel,
-  description,
-}) => {
-  return request({
-    url: "/supervisor/report",
-    method: "POST",
-    data: {
-      supervisorId,
-      address,
-      vAQILevel,
-      description,
-    },
-  });
-};
-
-// 查询自己提交的全部异常信息
-//这里的id是写死的，之后再修改
-export const ListExInformation = () => {
-  return request({
-    url: "/supervisor",
-    method: "GET",
-  });
-};
-
 //监督员登录
 export const login = ({ telephone, password }) => {
   return request({
@@ -55,7 +25,34 @@ export const register = ({ telephone, name, password }) => {
   });
 };
 
-//监督员注册
+// 添加举报信息
+export const insertSuperviseInfo = ({
+  supervisorId,
+  address,
+  vAQILevel,
+  description,
+}) => {
+  return request({
+    url: "/supervisor/report",
+    method: "POST",
+    data: {
+      supervisorId,
+      address,
+      vAQILevel,
+      description,
+    },
+  });
+};
+
+// 查询自己提交的全部异常信息
+export const ListExInformation = () => {
+  return request({
+    url: "/supervisor/all",
+    method: "GET",
+  });
+};
+
+//监督员个人信息
 export const info = () => {
   return request({
     url: "/supervisor/info",
